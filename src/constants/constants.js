5d6bedb4012eefec2e95729033abda730ce9bba5
Stanley Hayford <stanley_hayford@vienhealth.com>
2021-07-20 16:07:34 +0000

    Change link to production server url

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,4 @@
-export const SERVER_DEVELOPMENT_BASE_URL = 'http://localhost:5000/api/v1';
+// export const SERVER_DEVELOPMENT_BASE_URL = 'http://localhost:5000/api/v1';
+export const SERVER_DEVELOPMENT_BASE_URL = 'https://stanley-hayford-vienhealth.herokuapp.com/api/v1';
 export const VIENHEALTH_TOKEN_KEY = 'VIENHEALTH_TOKEN_KEY';
 export const VIENHEALTH_USER_KEY = 'VIENHEALTH_USER_KEY';
